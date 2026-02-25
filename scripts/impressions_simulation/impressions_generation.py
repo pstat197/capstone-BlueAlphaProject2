9054fc624a2b12dd6757a6fb4abaf5e961bf7fc7
Nathan Kim <nakim@ucsb.edu>
2026-02-25 15:40:52 -0800

    Updated impressions_generation using cpm

--- scripts/impressions_simulation/impressions_generation.py
+++ scripts/impressions_simulation/impressions_generation.py
@@ -4,7 +4,7 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from synth_input_classes.input_configurations import InputConfigurations
 
-def generate_impressions(config: InputConfigurations, spend_matrix: np.ndarray, coeffs: list) -> np.ndarray:
+def generate_impressions(config: InputConfigurations, spend_matrix: np.ndarray, cpm: list) -> np.ndarray:
     weeks = config.get_week_range()
     channels = len(config.get_channel_list())
     var = []
@@ -13,26 +13,29 @@ def generate_impressions(config: InputConfigurations, spend_matrix: np.ndarray, 
     
     std = np.sqrt(var)
 
-    means = np.zeros(channels)
+    std_scaled = std * spend_matrix
 
-    noise = np.random.normal(loc = means, scale = std, size = (weeks, channels))
+    means = np.zeros((weeks, channels))
 
-    impressions = spend_matrix * coeffs + noise
+    noise = np.random.normal(loc = means, scale = std_scaled, size = (weeks, channels))
+
+    impressions = ((spend_matrix / cpm) * 1000) + noise
 
     return impressions
 
-# TEST
+# # TEST
 
 # spend_matrix = np.array([[1, 2, 3],
-                        #  [4, 5, 7],
-                        #  [8, 9, 6]])
+#                          [4, 5, 7],
+#                          [8, 9, 6]])
 # weeks = 3
 # channels = 3
 # var = [0, 1, 2]
 # std = np.sqrt(var)
-# means = [0, 0, 0]
-# coefficients = [1, 2, 3]
-# noise = np.random.normal(loc=means, scale = std, size = (weeks, channels))
-# impressions = spend_matrix * coefficients + noise
+# std_scaled = std * spend_matrix
+# means = np.zeros((weeks, channels))
+# cpm = [1, 25, 48]
+# noise = np.random.normal(loc=means, scale = std_scaled, size = (weeks, channels))
+# impressions = ((spend_matrix / cpm) * 1000) + noise
 # print(impressions)
-
+# print(noise)
